89e3f3d784d4b05ebd46c5e2b506115697a673ba
Draco <loonghan@foxmail.com>
2024-02-20 13:25:40 +0800

    deleted some files

--- llm/route.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    ids = str(request.args.get('?'))
-    return 'Hello, World!' + ids
--- llm/test.py
+++
@@ -1,8 +0,0 @@
-
-
-referer = "http://localhost:8082/?userID=65cf55a99a47e8b0143139b0&cardID=65d0dc61bb41a9345fa71ede"
-card_id = referer.split("cardID=")[-1]
-user_id = referer.split("userID=")[-1].split("&")[0]
-
-print(card_id)
-print(user_id)

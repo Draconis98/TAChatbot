452df9f53a505c6ebae3b52d67fd4e56ac746872
Draco <loonghan@foxmail.com>
2024-02-21 17:28:35 +0800

    modified key and cert

--- vue-frontend/vite.config.js
+++ vue-frontend/vite.config.js
@@ -1,40 +1,44 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import * as fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-      AutoImport({
-          resolvers: [
-              ElementPlusResolver(),
-          ],
-      }),
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [
+                ElementPlusResolver(),
+            ],
+        }),
         Components({
             resolvers: [
                 ElementPlusResolver(),
             ],
         }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-        '/api': {
-            target: 'http://127.0.0.1:8080',
-            changeOrigin: true,
-        },
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     },
-	  host: '10.30.19.40',
-    port: 8081
-  }
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8080',
+                changeOrigin: true,
+            },
+        },
+        host: '10.30.19.40',
+        port: 8081,
+        https: true,
+        key: fs.readFile('/home/jss40/assistant/10.30.19.40-key.pem'),
+        cert: fs.readFile('/home/jss40/assistant/10.30.19.40.pem'),
+    }
 })

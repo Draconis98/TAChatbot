fde1043fc102d0727bd7c62c20c885d9ed0d9777
Draco <loonghan@foxmail.com>
2024-02-22 21:22:23 +0800

    add two field: Like and CreateAt

--- go-backend/internal/model/question.go
+++ go-backend/internal/model/question.go
@@ -6,4 +6,6 @@ type QA struct {
 	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	Question string             `json:"question" bson:"question"`
 	Answer   string             `json:"answer" bson:"answer"`
+	Like     int                `json:"like" bson:"like"` // 初始化为0， 1表示喜欢， 2表示不喜欢
+	CreateAt string             `json:"create_at" bson:"create_at"`
 }

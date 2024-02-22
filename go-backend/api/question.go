af2415589fa453650ee1a996ebfbadd29fbd5613
Draco <loonghan@foxmail.com>
2024-02-22 21:24:28 +0800

    add two field: Like and CreateAt

--- go-backend/api/question.go
+++ go-backend/api/question.go
@@ -22,9 +22,11 @@ func GetQuestion(r *gin.Context) {
 	_questionID, err := primitive.ObjectIDFromHex(questionID)
 	if err != nil {
 		r.JSON(http.StatusInternalServerError, gin.H{
-			"error":    err.Error(),
-			"question": nil,
-			"answer":   nil,
+			"error":     err.Error(),
+			"question":  nil,
+			"answer":    nil,
+			"likes":     nil,
+			"create_at": nil,
 		})
 		return
 	}
@@ -32,16 +34,20 @@ func GetQuestion(r *gin.Context) {
 	qa, err := q.GetQA(r, _questionID)
 	if err != nil {
 		r.JSON(http.StatusInternalServerError, gin.H{
-			"error":    err.Error(),
-			"question": nil,
-			"answer":   nil,
+			"error":     err.Error(),
+			"question":  nil,
+			"answer":    nil,
+			"likes":     nil,
+			"create_at": nil,
 		})
 		return
 	}
 
 	r.JSON(http.StatusOK, gin.H{
-		"error":    nil,
-		"question": qa.Question,
-		"answer":   qa.Answer,
+		"error":     nil,
+		"question":  qa.Question,
+		"answer":    qa.Answer,
+		"likes":     qa.Like,
+		"create_at": qa.CreateAt,
 	})
 }

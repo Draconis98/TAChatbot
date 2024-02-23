500fea8f7242fb5021da81784f2568b0849d2c40
Draco <loonghan@foxmail.com>
2024-02-23 15:12:10 +0800

    添加了对retry的应对

--- llm/registory/utils.py
+++ llm/registory/utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
@@ -20,12 +22,22 @@ class mongodb:
         if self.db[collection].find_one(query) is None:
             return self.db[collection].update_one(query, {"$set": query}, upsert=True)
 
+    def delete(self, collection, query):
+        return self.db[collection].delete_one(query)
+
 
 def record_question(request, message, partial_message):
+    # 获取当前时间
+    now = datetime.now()
+    # 格式化时间
+    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+
     db = mongodb("localhost", "llm")
     question_id = db.insert("questions",
                             {"question": message,
-                             "answer": partial_message}
+                             "answer": partial_message,
+                             "like": 0,
+                             "create_at": dt_string,}
                             ).inserted_id
     referer = request.headers["referer"]
     # 确定用户ID和卡片ID的类型
@@ -37,15 +49,16 @@ def record_question(request, message, partial_message):
 
     # 查看新问题与当前卡片记录的最后一个问题是否相同
     card = db.db["cards"].find_one({"_id": _card_id})
-    last_question_id = card["questions"][-1]
-    _last_question_id = ObjectId(last_question_id)
-    question = db.db["questions"].find_one({"_id": _last_question_id})
-    print(question["question"])
-    if question["question"] == message:
-        db.update("cards", {"_id": _card_id}, {"$pop": {"questions": -1}})
+    if len(card["questions"]) >= 1:
+        last_question_id = card["questions"][-1]
+        _last_question_id = ObjectId(last_question_id)
+        question = db.db["questions"].find_one({"_id": _last_question_id})
+        # print(question["question"])
+        if question["question"] == message:
+            db.update("cards", {"_id": _card_id}, {"$pop": {"questions": 1}})
 
-        db.update("cards", {"_id": _card_id}, {"$pop": {"content": -1}})
-        db.update("cards", {"_id": _card_id}, {"$pop": {"content": -1}})
+            db.update("cards", {"_id": _card_id}, {"$pop": {"content": 1}})
+            db.update("cards", {"_id": _card_id}, {"$pop": {"content": 1}})
 
     # print(question_id, card_id)
     db.update("users", {"_id": _user_id}, {"$push": {"questions": question_id}})

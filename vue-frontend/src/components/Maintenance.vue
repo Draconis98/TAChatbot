af71f836bfce9a01a0544abc6cb1a3de35da01bc
draco <draco@Dracos-Macbook-Pro.local>
2024-03-07 18:24:50 +0800

    添加了维护页面

---
+++ vue-frontend/src/components/Maintenance.vue
@@ -0,0 +1,38 @@
+<script setup>
+</script>
+
+<template>
+  <el-card class="card">
+    <div>
+      <h1>CALLMe平台正在维护</h1>
+      <h3>预计维护时间为 <u>2024年3月7日晚8点</u> 至 <u>2024年3月8日中午12点</u></h3>
+      <el-divider></el-divider>
+      <p>本次维护将会上线：助教/老师评论、热度更新、按最热排序、我的问题等功能</p>
+    </div>
+
+  </el-card>
+  <div class="bottom">
+    <p>如需帮助，请联系 serve@agileserve.org.cn</p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  display: flex;
+
+  width: 60%;
+  height: 60vh;
+  margin: auto;
+  margin-top: 20vh;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+}
+
+.bottom {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+</style>

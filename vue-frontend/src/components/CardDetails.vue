cfa83098228ffe4237ad007a2b506e3b45374cdf
draco <draco@Dracos-Macbook-Pro.local>
2024-03-11 21:30:29 +0800

    添加了代码高亮

--- vue-frontend/src/components/CardDetails.vue
+++ vue-frontend/src/components/CardDetails.vue
@@ -22,6 +22,8 @@ import {
   createNewQuestion,
 } from "@/composables/commonLogic.js";
 import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css';
 
 // variables
 
@@ -87,7 +89,19 @@ function submit() {
 }
 
 function parseMarkdown(text) {
-  const md = new MarkdownIt();
+  const md = new MarkdownIt({
+    highlight: function (str, lang) {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return '<pre class="hljs"><code>' +
+              hljs.highlight(lang, str, true).value +
+              '</code></pre>';
+        } catch (__) {
+        }
+      }
+      return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    }
+  });
   return md.render(text);
 }
 
